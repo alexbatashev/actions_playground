9baac0ef56a888a138a39bf8a4293624174fd76f
Alexander Batashev <alexbatashev@outlook.com>
2021-12-18 10:44:37 +0300

    +

--- .github/actions/cleanup/cleanup.js
+++ .github/actions/cleanup/cleanup.js
@@ -1,3 +1,3 @@
 const path = process.env.GITHUB_WORKSPACE + '/*';
 console.log('Cleaning ' + path)
-require('child_process').execSync('rm -rf ' path);
+require('child_process').execSync('rm -rf ' + path);
